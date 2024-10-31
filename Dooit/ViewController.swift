f780d906abea561048dcf3dc903fb93728559f61
Nurlybakyt <begaly606@gmail.com>
2024-10-31 15:45:06 +0500

    Initial Commit

---
+++ Dooit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dooit
+//
+//  Created by Dinmukhammed Begaly on 31.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
